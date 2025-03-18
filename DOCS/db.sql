66b58a3452790218e57cba742d25d841f089bee3
PedroDNRusso <pedrodnrusso@gmail.com>
2025-03-18 00:17:10 -0300

    Cadastro

--- DOCS/db.sql
+++ DOCS/db.sql
@@ -1,16 +1,18 @@
 Exemplo de DB 
 
-DROP database dd;
-CREATE DATABASE dd;
-USE dd;
+DROP database DigitalD;
+CREATE DATABASE DigitalD;
+USE DigitalD;
 
-CREATE TABLE pacientes(
+CREATE TABLE cli(
     id_pac INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome_paciente VARCHAR(255) NOT NULL,
+    nome_cli VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR (255) NOT NULL
 );
 
-insert into pacientes values (null, "Humberto", "humberto123@gmail.com", "humbigo231");
+insert into cli values (null, "Humberto", "humberto123@gmail.com", "humbigo231");
+insert into cli values (null, "Pedro", "russo007@gmail.com", "pedr007");
+insert into cli values (null, "Dutch", "dutch2022@gmail.com", "dutch22");
 
-select * from pacientes;
+select * from cli;
